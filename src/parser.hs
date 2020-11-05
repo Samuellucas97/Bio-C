0e8f91af452091e90af82fd3381df9257c81fa6d
Felipe Morais <moraisfelipe97@gmail.com>
2020-11-05 19:36:12 -0300

    Teste inicial

--- src/parser.hs
+++ src/parser.hs
@@ -9,9 +9,11 @@ import Terminals
 import System.Environment
 import Data.List
 
+import Control.Monad.IO.Class
+import System.IO.Unsafe
 -- program structure
 
-program :: Parsec [Token] st [Token]
+program :: ParsecT [Token] [(Token, Token)] IO([Token])
 program = do
         a <- structs
         b <- declarations
@@ -22,19 +24,19 @@ program = do
 
 -- aux
 
-extended_type :: Parsec [Token] st [Token]
+extended_type :: ParsecT [Token] [(Token, Token)] IO([Token])
 extended_type = (do 
         a <- typeToken <|> idToken
         return ([a]))
 
-extended_id :: Parsec [Token] st [Token]
+extended_id :: ParsecT [Token] [(Token, Token)] IO([Token])
 extended_id = (do 
         a <- idToken
         b <- square_brackets
         c <- attribute_access
         return ([a] ++ b ++ c))
 
-attribute_access :: Parsec [Token] st [Token]
+attribute_access :: ParsecT [Token] [(Token, Token)] IO([Token])
 attribute_access = (do 
         a <- dotToken
         b <- extended_id
@@ -42,33 +44,33 @@ attribute_access = (do
 
 -- structs
 
-structs :: Parsec [Token] st [Token]
+structs :: ParsecT [Token] [(Token, Token)] IO([Token])
 structs = (do
         first <- struct
         next <- remaining_struct
         return (first ++ next)) <|> (return [])
 
-struct :: Parsec [Token] st [Token]
+struct :: ParsecT [Token] [(Token, Token)] IO([Token])
 struct = (do
         a <- structToken
         b <- idToken
         c <- struct_block
         return([a]++[b]++c))
 
-struct_block :: Parsec [Token] st [Token]
+struct_block :: ParsecT [Token] [(Token, Token)] IO([Token])
 struct_block = do
         a <- beginScopeToken
         b <- attributes
         c <- endScopeToken
         return ([a] ++ b ++ [c])
 
-attributes :: Parsec [Token] st [Token]
+attributes :: ParsecT [Token] [(Token, Token)] IO([Token])
 attributes = (do
         first <- attribute
         next <- remaining_attribute
         return (first ++ next)) -- <|> (return [])
 
-attribute :: Parsec [Token] st [Token]
+attribute :: ParsecT [Token] [(Token, Token)] IO([Token])
 attribute = (do 
         a <- extended_type
         d <- square_brackets
@@ -76,35 +78,35 @@ attribute = (do
         c <- semiColonToken
         return (a ++ d ++ [b] ++ [c]))
 
-remaining_struct :: Parsec [Token] st [Token]
+remaining_struct :: ParsecT [Token] [(Token, Token)] IO([Token])
 remaining_struct = (do 
         a <- structs
         return (a)) <|> (return [])
 
-remaining_attribute :: Parsec [Token] st [Token]
+remaining_attribute :: ParsecT [Token] [(Token, Token)] IO([Token])
 remaining_attribute = (do 
         b <- attributes
         return (b)) <|> (return [])
 
-square_brackets :: Parsec [Token] st [Token]
+square_brackets :: ParsecT [Token] [(Token, Token)] IO([Token])
 square_brackets = (do 
         a <- beginSquareBracketToken
         b <- square_brackets_values
         c <- endSquareBracketToken
         return ([a] ++ b ++ [c])) <|> (return [])
 
-square_brackets_values :: Parsec [Token] st [Token]
+square_brackets_values :: ParsecT [Token] [(Token, Token)] IO([Token])
 square_brackets_values = (do
         first <- square_brackets_value
         next <- remaining_square_brackets_values
         return (first ++ next))
 
-square_brackets_value :: Parsec [Token] st [Token]
+square_brackets_value :: ParsecT [Token] [(Token, Token)] IO([Token])
 square_brackets_value = (do 
         b <- expr
         return (b))
 
-remaining_square_brackets_values :: Parsec [Token] st [Token]
+remaining_square_brackets_values :: ParsecT [Token] [(Token, Token)] IO([Token])
 remaining_square_brackets_values = (do 
         a <- commaToken
         b <- square_brackets_values
@@ -112,19 +114,19 @@ remaining_square_brackets_values = (do
 
 -- declarations
 
-declarations :: Parsec [Token] st [Token]
+declarations :: ParsecT [Token] [(Token, Token)] IO([Token])
 declarations = try (do
         first <- declaration
         second <- semiColonToken
         next <- remaining_declaration
         return (first ++ [second] ++ next)) <|> (return [])
 
-declaration :: Parsec [Token] st [Token]
+declaration :: ParsecT [Token] [(Token, Token)] IO([Token])
 declaration = try (do
         a <- varAssign <|> varDeclaration <|> constDeclaration
         return(a))
 
-constDeclaration :: Parsec [Token] st [Token]
+constDeclaration :: ParsecT [Token] [(Token, Token)] IO([Token])
 constDeclaration = do 
         a <- constToken
         b <- typeToken
@@ -135,16 +137,16 @@ constDeclaration = do
         --return(a:b:c:d:e ++ [f])
         return ([a] ++ [b] ++ [c] ++ [d] ++ e)
 
-literal :: Parsec [Token] st [Token]
+literal :: ParsecT [Token] [(Token, Token)] IO([Token])
 literal = do 
         a <- intToken <|> charToken  <|> booleanToken <|> stringToken <|> dnaToken <|> rnaToken <|> proteinToken 
         return ([a])
 
-remaining_declaration :: Parsec [Token] st [Token]
+remaining_declaration :: ParsecT [Token] [(Token, Token)] IO([Token])
 remaining_declaration = (do a <- declarations
                             return (a)) <|> (return [])
 
-main_ :: Parsec [Token] st [Token]
+main_ :: ParsecT [Token] [(Token, Token)] IO([Token])
 main_ = try (do
         a <- typeToken
         b <- mainToken
@@ -154,7 +156,7 @@ main_ = try (do
         h <- block
         return ( [a] ++ [b] ++ [c] ++ [e] ++ h))
 
-block :: Parsec [Token] st [Token]
+block :: ParsecT [Token] [(Token, Token)] IO([Token])
 block = do
         a <- beginScopeToken
         b <- stmts
@@ -162,13 +164,13 @@ block = do
         return ([a] ++ b ++ [c])
 
  
-stmts :: Parsec [Token] st [Token]
+stmts :: ParsecT [Token] [(Token, Token)] IO([Token])
 stmts = (do
           first <- stmt
           next <- remaining_stmts
           return (first ++ next)) <|> (return [])
 
-stmt :: Parsec [Token] st [Token]
+stmt :: ParsecT [Token] [(Token, Token)] IO([Token])
 stmt = try (do
         a <- try function_call  <|> varAssign <|> varDeclaration <|> return_
 
@@ -177,7 +179,7 @@ stmt = try (do
         a <- loop <|> condition
         return (a))
 
-function_call :: Parsec [Token] st [Token]
+function_call :: ParsecT [Token] [(Token, Token)] IO([Token])
 function_call = (do
         a <- idToken
         b <- beginBracketToken
@@ -186,12 +188,12 @@ function_call = (do
         return ([a] ++ [b] ++ c ++ [d])
         )
 
-opt_args :: Parsec [Token] st [Token]
+opt_args :: ParsecT [Token] [(Token, Token)] IO([Token])
 opt_args = (do
         a <- args
         return (a)) <|> (return [])
 
-args :: Parsec [Token] st [Token]
+args :: ParsecT [Token] [(Token, Token)] IO([Token])
 args = (do
         a <- expr
         return (a)
@@ -201,21 +203,22 @@ args = (do
         c <- args
         return(a ++ [b] ++ c))
 
-remaining_stmts :: Parsec [Token] st [Token]
+remaining_stmts :: ParsecT [Token] [(Token, Token)] IO([Token])
 remaining_stmts = (do 
                   a <- stmts
                   return (a)) <|> (return [])
 
-varDeclaration :: Parsec [Token] st [Token]
+varDeclaration :: ParsecT [Token] [(Token, Token)] IO([Token])
 varDeclaration = try (do
           a <- extended_type
           d <- square_brackets
           b <- idToken
+          updateState(symtable_insert (b, get_default_value a))
           c <- optAssign
           --d <- semiColonToken
           return (a ++ d ++[b] ++ c))
 
-optAssign :: Parsec [Token] st [Token]
+optAssign :: ParsecT [Token] [(Token, Token)] IO([Token])
 optAssign  = (do 
           a <- assignToken
           b <- array_def <|> expr
@@ -226,12 +229,12 @@ optAssign  = (do
           b <- expr
           return(a ++ b)) <|> (return [])
 
-assign_type :: Parsec [Token] st [Token]
+assign_type :: ParsecT [Token] [(Token, Token)] IO([Token])
 assign_type = (do 
           a  <- assignToken
           return([a]))
 
-array_def :: Parsec [Token] st [Token]
+array_def :: ParsecT [Token] [(Token, Token)] IO([Token])
 array_def = (do 
           a <- beginSquareBracketToken
           b <- elements
@@ -239,48 +242,27 @@ array_def = (do
           return ([a] ++ b ++ [c])
           )
 
-elements :: Parsec [Token] st [Token]
+elements :: ParsecT [Token] [(Token, Token)] IO([Token])
 elements = (do 
           a <- element
           b <- remaining_elements
           return (a ++ b))
 
-remaining_elements :: Parsec [Token] st [Token]
+remaining_elements :: ParsecT [Token] [(Token, Token)] IO([Token])
 remaining_elements = (do 
                   b <- commaToken
                   a <- elements
                   return ([b] ++ a)) <|> (return [])
 
-element :: Parsec [Token] st [Token]
+element :: ParsecT [Token] [(Token, Token)] IO([Token])
 element = (do 
           a <- array_def <|> expr
           return (a))
-{-
-expr :: Parsec [Token] st [Token]
-expr  = (do 
-        a <- simpleExpr <|> bracketExpr
-        return (a)) <|>  
-        (do 
-        a <- un_operator
-        b <- expr
-        return (a ++ b)) <|>  
-        (do 
-        a <- simpleExpr
-        c <- remaining_expr
-        return (a ++ c))
 
-remaining_expr :: Parsec [Token] st [Token]
-remaining_expr  = 
-        (do 
-        a <- simpleExpr
-        b <- bin_operator
-        c <- simpleExpr
-        return (a++ b ++ c))
--}
-expr :: Parsec [Token] st [Token]
+expr :: ParsecT [Token] [(Token, Token)] IO([Token])
 expr  = try bin_operation <|> un_operation <|> simpleExpr <|> bracketExpr
 
-bin_operation :: Parsec [Token] st [Token]
+bin_operation :: ParsecT [Token] [(Token, Token)] IO([Token])
 bin_operation  =  
         (do 
         a <- simpleExpr <|> bracketExpr
@@ -288,21 +270,21 @@ bin_operation  =
         c <- expr
         return (a ++ b ++ c))
 
-un_operation :: Parsec [Token] st [Token]
+un_operation :: ParsecT [Token] [(Token, Token)] IO([Token])
 un_operation  =  
         (do 
         b <- un_operator
         c <- expr
         return (b ++ c))
 
-simpleExpr :: Parsec [Token] st [Token]
+simpleExpr :: ParsecT [Token] [(Token, Token)] IO([Token])
 simpleExpr  = (do 
         a <- try function_call <|> extended_id
         return (a)) <|> (do 
         a <- literal
         return (a))
 
-varAssign :: Parsec [Token] st [Token]
+varAssign :: ParsecT [Token] [(Token, Token)] IO([Token])
 varAssign = try (do
           a <- extended_id
           --b <- squareBrackets
@@ -312,23 +294,23 @@ varAssign = try (do
           return (a ++ [c] ++ d))
 
 
-un_operator :: Parsec [Token] st [Token]
+un_operator :: ParsecT [Token] [(Token, Token)] IO([Token])
 un_operator  = (do 
         a <- notToken <|> minusToken
         return ([a]))
 
-bin_operator :: Parsec [Token] st [Token]
+bin_operator :: ParsecT [Token] [(Token, Token)] IO([Token])
 bin_operator  = (do 
         a <- minusToken <|> plusToken <|> divToken <|> multToken <|> modToken <|> powToken <|> andToken <|> orToken <|> equalToken <|> differentToken <|> greaterToken <|> lessToken <|> greaterEqualToken <|> lessEqualToken
         return ([a]))
 
-functions :: Parsec [Token] st [Token]
+functions :: ParsecT [Token] [(Token, Token)] IO([Token])
 functions = (do
         first <- function
         next <- remaining_function
         return (first ++ next)) <|> (return [])
 
-function :: Parsec [Token] st [Token]
+function :: ParsecT [Token] [(Token, Token)] IO([Token])
 function = try (do
         a <- extended_type
         b <- idToken
@@ -338,64 +320,64 @@ function = try (do
         h <- block
         return(a ++ [b] ++ [c] ++ d ++ [e] ++ h))
 
-remaining_function :: Parsec [Token] st [Token]
+remaining_function :: ParsecT [Token] [(Token, Token)] IO([Token])
 remaining_function = (do 
                       a <- functions
                       return (a)) <|> (return [])
 
 
-params :: Parsec [Token] st [Token]
+params :: ParsecT [Token] [(Token, Token)] IO([Token])
 params = (do
         first <- param
         next <- remaining_param
         return (first ++ next)) <|> (return [])
 
-param :: Parsec [Token] st [Token]
+param :: ParsecT [Token] [(Token, Token)] IO([Token])
 param = (do
         a <- extended_type
         b <- sb_param
         c <- idToken
         return(a ++ b ++ [c]))
 
-remaining_param :: Parsec [Token] st [Token]
+remaining_param :: ParsecT [Token] [(Token, Token)] IO([Token])
 remaining_param = (do 
                   a <- commaToken
                   b <- params
                   return ([a] ++ b)) <|> (return [])
 
 
-sb_param :: Parsec [Token] st [Token]
+sb_param :: ParsecT [Token] [(Token, Token)] IO([Token])
 sb_param = (do 
             a <- beginSquareBracketToken
             b <- sb_param_values
             c <- endSquareBracketToken
             return ([a] ++ b ++ [c])) <|> (return [])
 
-sb_param_values :: Parsec [Token] st [Token]
+sb_param_values :: ParsecT [Token] [(Token, Token)] IO([Token])
 sb_param_values = (do
         first <- sb_param_value
         next <- remaining_sb_param_value
         return (first ++ next)) <|> (return [])
 
-sb_param_value :: Parsec [Token] st [Token]
+sb_param_value :: ParsecT [Token] [(Token, Token)] IO([Token])
 sb_param_value = (do
         a <- expr
         return(a)) <|> (return [])
 
-remaining_sb_param_value :: Parsec [Token] st [Token]
+remaining_sb_param_value :: ParsecT [Token] [(Token, Token)] IO([Token])
 remaining_sb_param_value = (do 
                             a <- commaToken
                             b <- sb_param_values
                             return ([a] ++ b)) <|> (return [])
 
-loop :: Parsec [Token] st [Token]
+loop :: ParsecT [Token] [(Token, Token)] IO([Token])
 loop = (do 
         a <- whileToken 
         b <- bracketExpr
         e <- block
         return ([a] ++ b ++ e))
 
-condition :: Parsec [Token] st [Token]
+condition :: ParsecT [Token] [(Token, Token)] IO([Token])
 condition = (do 
         a <- ifToken 
         b <- bracketExpr
@@ -404,73 +386,54 @@ condition = (do
         f <- opt_condition
         return ([a] ++ b ++ e ++ f))
 
-opt_condition :: Parsec [Token] st [Token]
+opt_condition :: ParsecT [Token] [(Token, Token)] IO([Token])
 opt_condition = (do 
         a <- elseToken
         b <- block <|> condition
         return ([a] ++ b)) <|> (return [])
 
-bracketExpr :: Parsec [Token] st [Token]
+bracketExpr :: ParsecT [Token] [(Token, Token)] IO([Token])
 bracketExpr = (do 
         b <- beginBracketToken
         c <- expr
         d <- endBracketToken
         return ([b] ++ c ++ [d]))
 
-return_ :: Parsec [Token] st [Token]
+return_ :: ParsecT [Token] [(Token, Token)] IO([Token])
 return_ = (do 
         a <- returnToken 
         b <- expr
         return ([a] ++ b))
 
---program :: Parsec [Token] st [Token]
---program = do
---    a <- 
---    b <- idToken 
---    c <- main_
---    eof
---    return c
-
---main_ :: Parsec [Token] st [Token]
---main_ = do
---    a <- typeToken
---    b <- semiColonToken
---    return ([a] ++ [b])
---	a <- typeToken
---	b <- mainToken
---	c <- beginBracketToken
---	d <- 
---	e <- endBracketToken
---	f <- block
---	return (a ++ b ++ c ++ e ++ f)
-{-}
-block :: Parsec [Token] st [Token]
-block = do
-	a <- beginScopeToken
-	--b <- stmts
-	c <- endScopeToken
-	return ([a] ++ [c])
--}
 
 -- invocação do parser para o símbolo de partida 
 
-parser :: [Token] -> Either ParseError [Token]
-parser tokens = runParser program () "Error message" tokens
-{-
+get_default_value :: [Token] -> Token
+get_default_value ((Type p "int"):t) = (Int p 0)
+
+symtable_insert :: (Token,Token) -> [(Token,Token)] -> [(Token,Token)]
+symtable_insert symbol []  = [symbol]
+symtable_insert symbol symtable = symtable ++ [symbol]
+
+symtable_update :: (Token,Token) -> [(Token,Token)] -> [(Token,Token)]
+symtable_update _ [] = fail "variable not found"
+symtable_update (id1, v1) ((id2, v2):t) = 
+                               if id1 == id2 then (id1, v1) : t
+                               else (id2, v2) : symtable_update (id1, v1) t
+
+symtable_remove :: (Token,Token) -> [(Token,Token)] -> [(Token,Token)]
+symtable_remove _ [] = fail "variable not found"
+symtable_remove (id1, v1) ((id2, v2):t) = 
+                               if id1 == id2 then t
+                               else (id2, v2) : symtable_remove (id1, v1) t                               
+
+
+parser :: [Token] -> IO(Either ParseError [Token])
+parser tokens = runParserT program [] "Error message" tokens
+
+
 main :: IO ()
-main = case parser (getTokens "programaV0.pe") of
+main = case unsafePerformIO (parser (getTokens "simple_program.pe")) of
             { Left err -> print err; 
               Right ans -> print ans
             }
-
-
--}
-
-main :: IO ()
-main = do 
-  args <- getArgs
-  case parser (getTokens (head args)) of
-  {
-    Left err -> print err; 
-    Right ans -> print ans
-  }
--- src/terminals.hs
+++ src/terminals.hs
@@ -4,226 +4,230 @@ import Data.Functor.Identity
 import Lexer
 import Text.Parsec
 
+import Control.Monad.IO.Class
+import System.IO.Unsafe
+
 
 -- parsers para os tokens
 
-mainToken :: ParsecT [Token] st Identity Token
+mainToken :: ParsecT [Token] st IO(Token)
 mainToken = tokenPrim show update_pos get_token where
   get_token (Main p)    = Just (Main p)
   get_token _       = Nothing
 
-idToken :: ParsecT [Token] st Identity Token
+idToken :: ParsecT [Token] st IO(Token)
 idToken = tokenPrim show update_pos get_token where
   get_token (Var p s) = Just (Var p s)
   get_token _      = Nothing
 
-beginScopeToken :: ParsecT [Token] st Identity Token
+typeToken :: ParsecT [Token] st IO(Token)
+typeToken = tokenPrim show update_pos get_token where
+  get_token (Type p s) = Just (Type p s)
+  get_token _       = Nothing
+
+beginScopeToken :: ParsecT [Token] st IO(Token)
 beginScopeToken = tokenPrim show update_pos get_token where
   get_token (BeginScope p) = Just (BeginScope p)
   get_token _     = Nothing
 
-endScopeToken :: ParsecT [Token] st Identity Token
+endScopeToken :: ParsecT [Token] st IO(Token)
 endScopeToken = tokenPrim show update_pos get_token where
   get_token (EndScope p) = Just (EndScope p)
   get_token _   = Nothing
 
-beginBracketToken :: ParsecT [Token] st Identity Token
+beginBracketToken :: ParsecT [Token] st IO(Token)
 beginBracketToken = tokenPrim show update_pos get_token where
   get_token (BeginBracket p) = Just (BeginBracket p)
   get_token _     = Nothing
 
-endBracketToken :: ParsecT [Token] st Identity Token
+endBracketToken :: ParsecT [Token] st IO(Token)
 endBracketToken = tokenPrim show update_pos get_token where
   get_token (EndBracket p) = Just (EndBracket p)
   get_token _   = Nothing
 
-beginSquareBracketToken :: ParsecT [Token] st Identity Token
+beginSquareBracketToken :: ParsecT [Token] st IO(Token)
 beginSquareBracketToken = tokenPrim show update_pos get_token where
   get_token (BeginSquareBracket p) = Just (BeginSquareBracket p)
   get_token _     = Nothing
 
-endSquareBracketToken :: ParsecT [Token] st Identity Token
+endSquareBracketToken :: ParsecT [Token] st IO(Token)
 endSquareBracketToken = tokenPrim show update_pos get_token where
   get_token (EndSquareBracket p) = Just (EndSquareBracket p)
   get_token _   = Nothing
 
-semiColonToken :: Parsec [Token] st Token
+semiColonToken :: ParsecT [Token] st IO(Token)
 semiColonToken = tokenPrim show update_pos get_token where
   get_token (SemiColon p) = Just (SemiColon p)
   get_token _         = Nothing
 
-commaToken :: Parsec [Token] st Token
+commaToken :: ParsecT [Token] st IO(Token)
 commaToken = tokenPrim show update_pos get_token where
   get_token (Comma p) = Just (Comma p)
   get_token _         = Nothing
 {-
-colonToken :: Parsec [Token] st Token
+colonToken :: ParsecT [Token] st IO(Token)
 colonToken = tokenPrim show update_pos get_token where
   get_token (Colon p) = Just (Colon p)
   get_token _         = Nothing
 -}
 
-dotToken :: Parsec [Token] st Token
+dotToken :: ParsecT [Token] st IO(Token)
 dotToken = tokenPrim show update_pos get_token where
   get_token (Dot p) = Just (Dot p)
   get_token _         = Nothing
 
-ifToken :: Parsec [Token] st Token
+ifToken :: ParsecT [Token] st IO(Token)
 ifToken = tokenPrim show update_pos get_token where
   get_token (If p) = Just (If p)
   get_token _         = Nothing
 
-elseToken :: Parsec [Token] st Token
+elseToken :: ParsecT [Token] st IO(Token)
 elseToken = tokenPrim show update_pos get_token where
   get_token (Else p) = Just (Else p)
   get_token _         = Nothing
 
-assignToken :: ParsecT [Token] st Identity Token
+assignToken :: ParsecT [Token] st IO(Token)
 assignToken = tokenPrim show update_pos get_token where
   get_token (Assign p) = Just (Assign p)
   get_token _      = Nothing
 
-typeToken :: ParsecT [Token] st Identity Token
-typeToken = tokenPrim show update_pos get_token where
-  get_token (Type p s) = Just (Type p s)
-  get_token _       = Nothing
 
-returnToken :: ParsecT [Token] st Identity Token
+returnToken :: ParsecT [Token] st IO(Token)
 returnToken = tokenPrim show update_pos get_token where
   get_token (Return p) = Just (Return p)
   get_token _     = Nothing
 
-structToken :: ParsecT [Token] st Identity Token
+structToken :: ParsecT [Token] st IO(Token)
 structToken = tokenPrim show update_pos get_token where
   get_token (Struct p) = Just (Struct p)
   get_token _     = Nothing
 
-defToken :: ParsecT [Token] st Identity Token
+defToken :: ParsecT [Token] st IO(Token)
 defToken = tokenPrim show update_pos get_token where
   get_token (Def p) = Just (Def p)
   get_token _     = Nothing
 
-constToken :: ParsecT [Token] st Identity Token
+constToken :: ParsecT [Token] st IO(Token)
 constToken = tokenPrim show update_pos get_token where
   get_token (Const p) = Just (Const p)
   get_token _     = Nothing
 
-notToken :: ParsecT [Token] st Identity Token
+notToken :: ParsecT [Token] st IO(Token)
 notToken = tokenPrim show update_pos get_token where
   get_token (OpNot p) = Just (OpNot p)
   get_token _     = Nothing
 
-minusToken :: ParsecT [Token] st Identity Token
+minusToken :: ParsecT [Token] st IO(Token)
 minusToken = tokenPrim show update_pos get_token where
   get_token (Minus p) = Just (Minus p)
   get_token _     = Nothing
 
-plusToken :: ParsecT [Token] st Identity Token
+plusToken :: ParsecT [Token] st IO(Token)
 plusToken = tokenPrim show update_pos get_token where
   get_token (Plus p) = Just (Plus p)
   get_token _     = Nothing
 
-divToken :: ParsecT [Token] st Identity Token
+divToken :: ParsecT [Token] st IO(Token)
 divToken = tokenPrim show update_pos get_token where
   get_token (Div p) = Just (Div p)
   get_token _     = Nothing
 
-multToken :: ParsecT [Token] st Identity Token
+multToken :: ParsecT [Token] st IO(Token)
 multToken = tokenPrim show update_pos get_token where
   get_token (Mult p) = Just (Mult p)
   get_token _     = Nothing
 
-modToken :: ParsecT [Token] st Identity Token
+modToken :: ParsecT [Token] st IO(Token)
 modToken = tokenPrim show update_pos get_token where
   get_token (Mod p) = Just (Mod p)
   get_token _     = Nothing
 
-andToken :: ParsecT [Token] st Identity Token
+andToken :: ParsecT [Token] st IO(Token)
 andToken = tokenPrim show update_pos get_token where
   get_token (OpAnd p) = Just (OpAnd p)
   get_token _     = Nothing
 
-orToken :: ParsecT [Token] st Identity Token
+orToken :: ParsecT [Token] st IO(Token)
 orToken = tokenPrim show update_pos get_token where
   get_token (OpOr p) = Just (OpOr p)
   get_token _     = Nothing
 
-powToken :: ParsecT [Token] st Identity Token
+powToken :: ParsecT [Token] st IO(Token)
 powToken = tokenPrim show update_pos get_token where
   get_token (Pow p) = Just (Pow p)
   get_token _     = Nothing
 
-equalToken :: ParsecT [Token] st Identity Token
+equalToken :: ParsecT [Token] st IO(Token)
 equalToken = tokenPrim show update_pos get_token where
   get_token (Equal p) = Just (Equal p)
   get_token _     = Nothing
 
-differentToken :: ParsecT [Token] st Identity Token
+differentToken :: ParsecT [Token] st IO(Token)
 differentToken = tokenPrim show update_pos get_token where
   get_token (Different p) = Just (Different p)
   get_token _     = Nothing
 
-greaterToken :: ParsecT [Token] st Identity Token
+greaterToken :: ParsecT [Token] st IO(Token)
 greaterToken = tokenPrim show update_pos get_token where
   get_token (Greater p) = Just (Greater p)
   get_token _     = Nothing
 
-lessToken :: ParsecT [Token] st Identity Token
+lessToken :: ParsecT [Token] st IO(Token)
 lessToken = tokenPrim show update_pos get_token where
   get_token (Less p) = Just (Less p)
   get_token _     = Nothing
 
-greaterEqualToken :: ParsecT [Token] st Identity Token
+greaterEqualToken :: ParsecT [Token] st IO(Token)
 greaterEqualToken = tokenPrim show update_pos get_token where
   get_token (GreaterOrEqual p) = Just (GreaterOrEqual p)
   get_token _     = Nothing
 
-lessEqualToken :: ParsecT [Token] st Identity Token
+lessEqualToken :: ParsecT [Token] st IO(Token)
 lessEqualToken = tokenPrim show update_pos get_token where
   get_token (LessOrEqual p) = Just (LessOrEqual p)
   get_token _     = Nothing
 
-whileToken :: ParsecT [Token] st Identity Token
+whileToken :: ParsecT [Token] st IO(Token)
 whileToken = tokenPrim show update_pos get_token where
   get_token (While p) = Just (While p)
   get_token _     = Nothing
 
-intToken :: ParsecT [Token] st Identity Token
+intToken :: ParsecT [Token] st IO(Token)
 intToken = tokenPrim show update_pos get_token where
   get_token (Int p s) = Just (Int p s)
   get_token _       = Nothing
 
-floatToken :: ParsecT [Token] st Identity Token
+floatToken :: ParsecT [Token] st IO(Token)
 floatToken = tokenPrim show update_pos get_token where
   get_token (Float p s) = Just (Float p s)
   get_token _       = Nothing
 
-charToken :: ParsecT [Token] st Identity Token
+charToken :: ParsecT [Token] st IO(Token)
 charToken = tokenPrim show update_pos get_token where
   get_token (Char p s) = Just (Char p s)
   get_token _       = Nothing
 
-booleanToken :: ParsecT [Token] st Identity Token
+booleanToken :: ParsecT [Token] st IO(Token)
 booleanToken = tokenPrim show update_pos get_token where
   get_token (Boolean p s) = Just (Boolean p s)
   get_token _       = Nothing
 
-dnaToken :: ParsecT [Token] st Identity Token
+dnaToken :: ParsecT [Token] st IO(Token)
 dnaToken = tokenPrim show update_pos get_token where
   get_token (Dna p s) = Just (Dna p s)
   get_token _       = Nothing
 
-rnaToken :: ParsecT [Token] st Identity Token
+rnaToken :: ParsecT [Token] st IO(Token)
 rnaToken = tokenPrim show update_pos get_token where
   get_token (Rna p s) = Just (Rna p s)
   get_token _       = Nothing
 
-proteinToken :: ParsecT [Token] st Identity Token
+proteinToken :: ParsecT [Token] st IO(Token)
 proteinToken = tokenPrim show update_pos get_token where
   get_token (Protein p s) = Just (Protein p s)
   get_token _       = Nothing 
 
-stringToken :: ParsecT [Token] st Identity Token
+stringToken :: ParsecT [Token] st IO(Token)
 stringToken = tokenPrim show update_pos get_token where
   get_token (String p s) = Just (String p s)
   get_token _       = Nothing
